fb1bcffbdcde247d7ed8f7cb10303d1e9a51779a
caoshiqian <1661473920@qq.com>
2022-05-16 18:41:47 +0800

    提交登录接口测试用例

--- test_case/test_login/test_login.py
+++ test_case/test_login/test_login.py
@@ -12,11 +12,10 @@ from common.handle_request import HandleRequest
 
 
 class TestLogin:
-    @allure.feature('智能交通')
+    @allure.feature('Jugo-Account')
     @allure.story('登录')
     @allure.step('步骤：测试登录功能')
-    @allure.title("测试登录功能正常")
-    @pytest.mark.smoke
+    @pytest.mark.p0
     @pytest.mark.parametrize("args", YamlUtil(os.getcwd()+"/test_case/test_login/login.yaml").read_yaml())
     #@pytest.mark.flaky(reruns=2, reruns_delay=5)
     def test_login(self, args):
@@ -32,19 +31,6 @@ class TestLogin:
         assert result.json()['code'] == args['login']['code']
         assert result.json()['codeRemark'] == "成功"
         assert assertpy.assert_that(result.json()['codeRemark']).is_type_of(str)
-
-    @pytest.mark.parametrize("login_data", HandleExcel("login.xlsx", "login").read_excel())
-    def test_login2(self, login_data):
-        print("请求参数为：", login_data)
-        request_url = login_data['url']
-        request_method = login_data['method']
-        request_data = login_data['request']
-        result_code = login_data['expected']
-        request_result = HandleRequest().request(method=request_method, url=request_url, data=request_data, headers={'Content-Type': "application/json"})
-
-        # 对响应结果断言
-        assert request_result.json()['code'] == result_code
-
 
 if __name__ == '__main__':
     pytest.main(['-s'])
